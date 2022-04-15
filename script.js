33ac8b60dcc89e8d9a3f9314a0e32adc26f57248
Ericchuk <47712998+Ericchuk@users.noreply.github.com>
2022-04-15 15:52:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let email = document.querySelector("input").value;
+let error = document.querySelector(".error");
+let button = document.querySelector(".button");
+
+// button.addEventListener("click", checkInput(){
+
+// })
